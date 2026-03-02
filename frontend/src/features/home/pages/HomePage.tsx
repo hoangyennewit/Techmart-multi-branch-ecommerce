0f38d97f8599ce0d89c67916c3d4b374f0f3157c
hoangyennewit <hohoangyen2k5@gmail.com>
2026-03-02 10:43:05 +0700

    thêm tailwind để làm giao diện

---
+++ frontend/src/features/home/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export const Homepage = () => {
+    return (<div>
+            <h1>Welcome to the Homepage!</h1>
+            <p>This is the main landing page of our application.</p>
+        </div>
+    );
+};
