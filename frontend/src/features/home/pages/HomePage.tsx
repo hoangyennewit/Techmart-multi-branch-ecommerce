658caf25a2a0b1de807697fee7d602b89eff613c
Vũ Nguyễn <nhvu2005@gmail.com>
2026-03-03 09:27:33 +0700

    cấu hình tạm thời trang chủ và tài khoản

--- frontend/src/features/home/pages/HomePage.tsx
+++ frontend/src/features/home/pages/HomePage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-export const Homepage = () => {
+export const HomePage = () => {
     return (<div>
-            <h1>Welcome to the Homepage!</h1>
-            <p>This is the main landing page of our application.</p>
+            <h1>Home Page</h1>
         </div>
     );
 };
