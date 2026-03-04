20c41ee037153bc243c94ff4165beddd4b74c183
Vũ Nguyễn <nhvu2005@gmail.com>
2026-03-05 06:31:52 +0700

    Giao diện trang chủ chưa đăng nhập

--- frontend/src/features/home/components/PromoBanner.tsx
+++ frontend/src/features/home/components/PromoBanner.tsx
@@ -14,9 +14,9 @@ interface PromoBannerProps {
 const promoBanners: PromoBannerProps[] = [
     {
         id: 1,
-        title: "Sắc Màu Rực Rỡ - Pin Khủng Cả Ngày",
+        title: "Sắc Màu Rực Rỡ Pin Khủng Cả Ngày",
         description: "Sở hữu Iphone 14 và Iphone 14 Plus với màn hình lớn hơn, hệ thống camera kép ấn tượng và thời gian pin bền bỉ nhất",
-        imageUrl: "https://cdn.tgdd.vn/2023/09/banner/800-200-800x200-1.png",
+        imageUrl: "https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone-14-iPhone-14-Plus-hero-220907_Full-Bleed-Image.jpg.xlarge_2x.jpg",
         link: "https://www.thegioididong.com/dtdd/iphone-14"
     }];
 
@@ -25,27 +25,30 @@ export const PromoBanner = () => { //tham số sau này khi có backend (props: 
         return null;
     }
     return (
-        <Swiper
-            modules={[Autoplay, Pagination]}
-            pagination={{clickable: true}}
-            autoplay={{delay: 3000}}
-            loop= {promoBanners.length > 1}
-        >
-            {promoBanners.map((banner) => (
-                <SwiperSlide key={banner.id}>
-                    <section className="">
-                        <div>
-                            <h2>{banner.title}</h2>
-                            <p>{banner.description}</p>
-                        </div>
-                        <button
-                            onClick={() => banner.link && (window.location.href = banner.link)}
-                            className="btn btn-primary">
-                                Mua ngay</button>
-                        <img src={banner.imageUrl} alt={`Promo Banner ${banner.id}`}/>
-                    </section>     
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <div className="w-full max-w-20xl mx-auto px-50 py-5">
+            <Swiper
+                modules={[Autoplay, Pagination]}
+                pagination={{clickable: true}}
+                autoplay={{delay: 3000}}
+                loop= {promoBanners.length > 1}
+                className="mySwiper w-full h-60 mt-4"
+            >
+                {promoBanners.map((banner) => (
+                    <SwiperSlide key={banner.id}>
+                        <section className="bg-indigo-950 text-white p-7 rounded-lg flex items-center justify-between gap-6">
+                            <div className="">
+                                <h2 className="text-5xl font-bold">{banner.title}</h2>
+                                <p className="mt-2 text-zinc-500">{banner.description}</p>
+                            </div>
+                            <button
+                                onClick={() => banner.link && (window.location.href = banner.link)}
+                                className="btn btn-primary bg-orange-600 text-white font-bold px-6 py-2 rounded-full text-xs hover:bg-green-700 transition-all whitespace-nowrap shadow-sm uppercase tracking-wider">
+                                    Mua ngay</button>
+                            <img className="w-70 h-45 object-cover rounded-md" src={banner.imageUrl} alt={`Promo Banner ${banner.id}`}/>
+                        </section>     
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
     );
 }
