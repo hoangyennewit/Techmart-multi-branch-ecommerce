19a65e8b7279302dea43647753d20b19d703b37b
Vũ Nguyễn <nhvu2005@gmail.com>
2026-03-05 12:16:34 +0700

    Componet thông số kỹ thuật

--- frontend/src/features/products/pages/ProductDetail.tsx
+++
@@ -1,9 +0,0 @@
-import {useParams} from "react-router-dom";
-export const ProductDetail = () => {
-    const {id} = useParams();
-    return (
-        <div>
-            <h1>Product Detail Page</h1>
-        </div>
-    );
-};
