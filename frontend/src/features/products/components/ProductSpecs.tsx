189099b1af5943f1573e31066241939d0a377c91
hoangyennewit <hohoangyen2k5@gmail.com>
2026-03-05 12:16:34 +0700

    Componet thông số kỹ thuật

---
+++ frontend/src/features/products/components/ProductSpecs.tsx
@@ -0,0 +1,73 @@
+export const ProductSpecs = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-between mb-6">
+                <h2 className="text-2xl font-bold mb-4">Thông số kỹ thuật</h2>
+                <a href="#" className="text-blue-500 hover:underline mb-4 block">Xem tất cả</a>
+            </div>
+            <div>
+                <table className="table-auto border-collapse">
+                    <tbody>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Kích thước màn hình</td>
+                            <td className="py-2 px-4 w-150">6.5 inch, AMOLED, 120Hz</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Công nghệ màn hình</td>
+                            <td className="py-2 px-4">6.5 inch, AMOLED, 120Hz</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Camera sau</td>
+                            <td className="py-2 px-4">Chính 64MP + Phụ 8MP + Phụ 2MP</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Camera trước</td>
+                            <td className="py-2 px-4">16MP</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Chipset</td>
+                            <td className="py-2 px-4">Snapdragon 888</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Công nghệ NFC</td>
+                            <td className="py-2 px-4">Có</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Dung lượng RAM</td>
+                            <td className="py-2 px-4">6GB</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Bộ nhớ trong</td>
+                            <td className="py-2 px-4">128GB</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Pin</td>
+                            <td className="py-2 px-4">5000mAh, sạc nhanh 18W</td>
+                        </tr>
+
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Thẻ SIM</td>
+                            <td className="py-2 px-4">2 SIM (Nano-SIM)</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Hệ điều hành</td>
+                            <td className="py-2 px-4">Android 11</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Độ phân giải màn hình</td>
+                            <td className="py-2 px-4">2400 x 1080 pixels</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Tính năng màn hình</td>
+                            <td className="py-2 px-4">NULL</td>
+                        </tr>
+                        <tr className="border-b">
+                            <td className="py-2 px-4 font-bold bg-gray-200">Loại CPU</td>
+                            <td className="py-2 px-4">Snapdragon 888</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
