fed66d133e7594d8017559570365253226cc1e4c
hoangyennewit <hohoangyen2k5@gmail.com>
2026-03-08 08:13:55 +0700

    cấu hình interface cho product

--- frontend/src/features/products/types.ts
+++ frontend/src/features/products/types.ts
@@ -1,10 +1,46 @@
-export interface Product {
-    id: number;
+export interface ProductImage {
+    id: string;
+    url: string;
+}
+export interface ProductColor {
+    id: string;
+    name: string;
+    hex: string;
+}
+export interface ProductVariant {
+    id: string;
     name: string;
     price: number;
-    originalPrice: number;
-    imageUrl: string;
-    color: string[]; // Nang cap sau
-    rating: number; 
-    specs?: string[]; //Postgresql
+    stock: number;
+}
+export interface ProductSpec {
+    name: string;
+    value: string;
+}
+export interface ProductComment {
+    id: string;
+    userName: string;
+    content: string;
+    stars: Number;
+    createAt: String;
+}
+export interface Product {
+    id: string;
+    name: string;
+    description?: string;
+    price: number;
+    originalPrice?: number;
+    discount?: number;
+
+    images: ProductImage[]; 
+    colors: ProductColor[];
+    variants: ProductVariant[];
+    specs: ProductSpec[]
+
+    stock: number;
+    rating: number;
+    sold?: number;
+    createdAt?: string; 
+
+    comments?: ProductComment[];
 }
