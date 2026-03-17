8361f5ef878e81e17c3af3d70fe15aeef71d746e
hoangyennewit <hohoangyen2k5@gmail.com>
2026-03-17 17:55:58 +0700

    Thêm chức năng lọc sản phẩm theo loại sản phẩm

---
+++ frontend/src/features/products/pages/CategoryPage.tsx
@@ -0,0 +1,12 @@
+import {Header} from "../../../components/Header";
+import {CategoryBar} from "../../../components/CategoryBar";
+import {ProductCard} from "../../../components/product/ProductCard";
+
+export const CategoryPage = () => {
+    return (
+        <div>
+            <Header />
+            <CategoryBar />
+        </div>
+    );
+};
