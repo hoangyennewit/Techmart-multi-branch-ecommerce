af670ae1251cb2ec5d923becc11422a0ded9eea6
Vũ Nguyễn <nhvu2005@gmail.com>
2026-04-19 18:09:25 +0700

    feat: chuẩn hóa hiển thị ảnh từ cloudinary và fix routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app';
-import {connectDB} from './config/database';
+import { connectDB } from './config/database';
 import authRoutes from './routes/authRoutes';
+import productRoutes from './routes/product.routes'; // 
+
 import passport from 'passport';
 import './config/passport';
 
@@ -11,11 +13,15 @@ app.use(passport.initialize());
 app.use('/api/auth', authRoutes);
 
 
+app.use('/api/products', productRoutes);
+
 const PORT = process.env.PORT || 5000;
-const start = async() => {
-    await connectDB();
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
+
 start();
